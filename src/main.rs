9311601fc8a8849371dc55284518f4d9245a3e54
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2019-03-28 23:55:59 +0100

    Restart the whole process on sync call for now.

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,8 @@ use std::{
   time::Duration,
   sync::{mpsc, Arc},
   fs::File,
-  io::prelude::*,
-  process::{exit, Command, ExitStatus},
+  io::{BufWriter, Write},
+  process::{exit, Command, Stdio, ExitStatus},
   path::{PathBuf, Path}
 };
 
@@ -29,17 +29,15 @@ mod args;
 use crate::args::{get_options, get_matches};
 
 #[cfg(not(target_os = "windows"))]
-fn init_tray(temp_dir: String) {
+fn init_tray() {
   info!("\"tray\" is currently not supported on your system.");
 }
 
 #[cfg(target_os = "windows")]
-fn init_tray(temp_dir: String) {
+fn init_tray() {
   thread::spawn(move || {
     if let Ok(mut app) = systray::Application::new() {
       let window = unsafe { kernel32::GetConsoleWindow() };
-
-      let (root, remote_root, _, checkers, tps_limit) = get_options();
 
       if window != ptr::null_mut() {
         unsafe {
@@ -52,11 +50,32 @@ fn init_tray(temp_dir: String) {
         Err(e) => println!("{}", e)
       };
 
+      //Restart the whole process on sync call for now.
       app.add_menu_item(&"Sync".to_string(), move |_| {
-        match sync(&remote_root, &temp_dir, &root, checkers, tps_limit) {
-          Ok(_) => (),
-          Err(_) => error!("Sync failed.")
+        let mut process = Command::new("powershell")
+          .args(&["-Command", "-"])
+          .stdin(Stdio::piped())
+          .spawn().expect("Could not start powershell.");
+
+        {
+          let mut out_stdin = process.stdin.as_mut().expect("Could not collect stdin.");
+          let mut writer = BufWriter::new(&mut out_stdin);
+
+          let current_path = env::current_exe().expect("Could not get startup path of executable.");
+
+          //Stop current instance of rclone_ignore.
+          writer.write_all("Stop-Process -processname rclone_ignore;".as_bytes())
+            .expect("Could not write to powershell process.");
+
+          //Start new instance of rclone_ignore.
+          writer.write_all(
+            format!("Start-Process -FilePath \"{}\" -ArgumentList \"{}\";",
+            current_path.display(), env::args().skip(1).collect::<Vec<_>>().join(" ")).as_bytes()
+          )
+            .expect("Could not write to powershell process.");
         }
+
+        process.wait().expect("Could not start powershell process.");
       }).ok();
 
       app.add_menu_item(&"Show".to_string(), move |_| {
@@ -200,7 +219,7 @@ fn main() -> Result<(), Box<dyn Error>> {
   let mut legal_paths = get_included_paths(&root);
 
   if cfg!(target_os = "windows") {
-    init_tray(dir.display().to_string());
+    init_tray();
   }
 
   sync(&remote_root, &dir.display().to_string(), root, checkers, tps_limit, mod_time)?;
